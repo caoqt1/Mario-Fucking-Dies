042dbb098ca89b27dac47b4eca17dba33dd07df9
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-09-01 15:11:07 -0600

    Update rigid_body.h

--- src/game/rigid_body.h
+++ src/game/rigid_body.h
@@ -83,7 +83,7 @@ struct RigidBody {
 struct PhysicsConstraint {
     struct RigidBody *body;
 
-
+    
 };
 
 typedef u8 MeshEdge[2];
@@ -103,6 +103,7 @@ struct Collision {
     struct RigidBody *body2;
     struct CollisionPoint points[50];
     u32 numPoints;
+    u8 isCeilOrWall;
 };
 
 struct MeshInfo {
