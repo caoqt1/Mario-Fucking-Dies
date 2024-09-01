1b1bc1c9902ce4c90af1d9af7590589698edfd8a
caoqt <51724932+caoqt1@users.noreply.github.com>
2024-09-01 15:44:57 -0600

    Update sample_cube.inc.c

--- src/game/behaviors/sample_cube.inc.c
+++ src/game/behaviors/sample_cube.inc.c
@@ -2,12 +2,18 @@
 #include "actors/group0.h"
 #include "src/game/rigid_body.h"
 
-Vec3f Collider_Size = {37.0f, 20.0f, 25.0f};
+/*Vec3f Collider_Size = {37.0f, 20.0f, 25.0f};
 
 Vec3f M_Size = {17.0f, 17.0f, 17.0f};
 
 Vec3f Tiny_Size = {17.0f, 17.0f, 17.0f};
-Vec3f Arm_Size = {11.0f, 11.0f, 11.0f};
+Vec3f Arm_Size = {11.0f, 11.0f, 11.0f};*/
+
+Vec3f Collider_Size = {22.2f, 12.0f, 15.0f};
+Vec3f M_Size = {10.2f, 10.2f, 10.2f};
+
+Vec3f Tiny_Size = {10.2f, 10.2f, 10.2f};
+Vec3f Arm_Size = {6.6f, 6.6f, 6.6f};
 
 f32 ModelScale = 0.6;
 
